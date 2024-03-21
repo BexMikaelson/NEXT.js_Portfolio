f86d21ed01b6d3d96b323142b06daf197cc66784
Heidi sjöberg <heidi.alexx8@gmail.com>
2024-03-21 10:54:32 +0100

    removing Gallerycomponent

--- app/components/ProjectsSection.jsx
+++ app/components/ProjectsSection.jsx
@@ -1,7 +1,6 @@
 "use client";
 import React, { useState, useRef } from "react";
 import ProjectCard from "./ProjectCard";
-import ImageGallery from "./ImageGallery";
 
 const projectsData = [
     {
